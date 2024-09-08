11b8e2726b94d9a9d50402e6d15ad476641781cf
Phillip Porch <ppporch@theporch.com>
2024-09-08 09:19:37 -0500

    Initial Commit

---
+++ WordCraft/WordCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordCraftApp.swift
+//  WordCraft
+//
+//  Created by Phillip Porch on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WordCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
